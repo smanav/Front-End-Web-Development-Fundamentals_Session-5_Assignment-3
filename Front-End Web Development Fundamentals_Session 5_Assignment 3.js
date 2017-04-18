945625c1b4a597e185d98bf4a3c21954a303171a
smanav <smanav1991@gmail.com>
2017-04-18 16:06:17 +0530

    Add files via upload

---
+++ Front-End Web Development Fundamentals_Session 5_Assignment 3.js
@@ -0,0 +1,14 @@
+// Here is the script example
+
+var Employee = {
+	            Name: "Sanjay Patel", 
+	            Age:35, 
+	            Salary: "7.5 LPA", 
+	            Address: {
+	            	City: "Pune",
+	            	State: "Maharashtra",
+	            	Pincode: 423203
+	                     }
+	            };
+
+console.log(Employee);
